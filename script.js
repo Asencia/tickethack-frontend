e867fc460d93647922adc136d39006290fda873c
Younes <younes.benabbou@gmail.com>
2025-09-24 10:14:44 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+document.querySelector("#search").addEventListener("click", () => {
+    fetch("http://localhost:3000/trips", {
+        method: "GET"
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data){
+            document.querySelector("#result").innerHTML = `
+            <div class = "trip">${data[0].departure} > ${data[0].arrival}</div>
+            <div class = "hour">${data[0].date}</div>
+            <div class = "price">${data[0].price}€</div>
+            <div class = "book"><button id="btn-book"> Book </button></div>
+                `;
+        }
+
+            for (let i = 0 ; i < data.length; i++) {
+                document.querySelector("#result").innerHTML += `
+                <div class = "trip">${data[i].departure} > ${data[i].arrival}</div>
+                <div class = "hour">${data[i].date}</div>
+                <div class = "price">${data[i].price}€</div>
+                <div class = "book"><button id="btn-book"> Book </button></div>
+                    `;
+            }
+
+    })
+})
